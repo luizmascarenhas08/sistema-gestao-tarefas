7cc18d7b934627206413c091b819e03fa7bc20f6
Luiz Emanuel <luiz.mascarenhas80@gmail.com>
2026-02-03 11:19:58 -0300

    Apply auth middleware to all task routes

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const taskController = require('../controllers/taskController');
 
-router.use(auth);
-router.post('/', taskController.createTask);
-router.get('/', taskController.getTasks);
-router.get('/:id', taskController.getTaskById);
-router.put('/:id', taskController.updateTask);
-router.delete('/:id', taskController.deleteTask);
+// Rotas protegidas (auth aplicado rota por rota)
+router.post('/', auth, taskController.createTask);
+router.get('/', auth, taskController.getTasks);
+router.get('/:id', auth, taskController.getTaskById);
+router.put('/:id', auth, taskController.updateTask);
+router.delete('/:id', auth, taskController.deleteTask);
 
 module.exports = router;
