ca63269cb22017b4a7240ea6bd8b8d93ab6b6640
Luiz Emanuel <luiz.mascarenhas80@gmail.com>
2026-02-03 11:26:50 -0300

    Add CORS preflight response for task routes

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const taskController = require('../controllers/taskController');
 
-// Rotas protegidas (auth aplicado rota por rota)
+// Responde ao preflight sem exigir token
+router.options('/', (req, res) => {
+  res.sendStatus(200);
+});
+router.options('/:id', (req, res) => {
+  res.sendStatus(200);
+});
+
+// Rotas protegidas
 router.post('/', auth, taskController.createTask);
 router.get('/', auth, taskController.getTasks);
 router.get('/:id', auth, taskController.getTaskById);
